d17a2308ff903b459b6c9310fd6d42eb0e051544
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-07-17 12:16:20 +0200

    Add error handling in statistics sender

--- statsSend/teamCity/teamCityStatisticsSender.py
+++ statsSend/teamCity/teamCityStatisticsSender.py
@@ -17,10 +17,19 @@ class TeamCityStatisticsSender:
 
     async def send(self):
         if ("report_categories" in dir(self.reporter)):
-            categories = [build_configuration.toCategory() async for build_configuration in self.project.retrieve_build_configurations()]
-            self.reporter.report_categories(categories)
+            try:
+                categories = [build_configuration.toCategory() async for build_configuration in self.project.retrieve_build_configurations()]
+                self.reporter.report_categories(categories)
+            except Exception as err:
+                eprint("Error sending categories" + err)
 
-        async for build_configuration in self.project.retrieve_build_configurations():
-            async for build_run in build_configuration.retrieve_build_runs_since_timestamp(self.since_timestamp):
-                job = build_run.toJob()
-                self.reporter.report_job(job)
+        try:
+            async for build_configuration in self.project.retrieve_build_configurations():
+                async for build_run in build_configuration.retrieve_build_runs_since_timestamp(self.since_timestamp):
+                    try:
+                        job = build_run.toJob()
+                        self.reporter.report_job(job)
+                    except Exception as err:
+                        eprint("Error reporting job" + err)
+        except Exception as err:
+            eprint("Error reporting jobs" + err)
