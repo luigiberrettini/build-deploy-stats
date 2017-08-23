7d1b7875cdb69082c634ca815e892b5ec4f9ebe2
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-08-23 16:54:50 +0200

    Use a different discovery_rule_key for each stats sender

--- reporting/zabbixReporter.py
+++ reporting/zabbixReporter.py
@@ -13,15 +13,18 @@ class ZabbixReporter:
     def __init__(self, settings):
         self.sender = ZabbixSender(settings['server'], settings['port'])
         self.hostname = settings['hostname']
-        self.discovery_rule_key = settings['discovery_rule_key']
+        self.discovery_rule_key_prefix = settings['discovery_rule_key_prefix']
         self.category_tool_macro = settings['category_tool_macro']
         self.category_context_macro = settings['category_context_macro']
 
     def report_categories(self, categories):
+        if (len(categories) <= 0):
+            return
         to_dict = lambda x: { self.category_tool_macro: '{:s}'.format(x.tool), self.category_context_macro: '{:s}'.format(x.context) }
         category_dict_list = list(map(to_dict, categories))
+        discovery_rule_key = '{:s}{:s}'.format(self.discovery_rule_key_prefix, categories[0].tool).lower()
         discovery_rule_payload = json.dumps({ 'data': category_dict_list })
-        packet = [ ZabbixMetric(self.hostname, self.discovery_rule_key, discovery_rule_payload) ]
+        packet = [ ZabbixMetric(self.hostname, discovery_rule_key, discovery_rule_payload) ]
         self.sender.send(packet)
 
     def report_activity(self, activity):
